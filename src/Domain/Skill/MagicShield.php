5201382f3c29c60c7a478e1785de10e3e99d897a
Victor Todoran <victor.todoran@evozon.com>
2021-04-17 20:36:57 +0300

    Document Skills.

--- src/Domain/Skill/MagicShield.php
+++ src/Domain/Skill/MagicShield.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace App\Domain\Skill;
 
+/**
+ * MagicShield skill causes only half of the damage when an enemy attacks
+ */
 class MagicShield extends AbstractSkill
 {
     public const IDENTIFIER = 'magic_shield';
@@ -12,6 +15,22 @@ class MagicShield extends AbstractSkill
         return $attackValue;
     }
 
+    /*
+     * Given the skill's description there are two ways to go abt this.
+     * 1. Alter the 'candidate' damage in the current attack.
+     * 2. Increase the defence value of the character to decrease the impact of the candidate damage.
+     * For opinionated reasons the implementation follows the second option.
+     * This however comes with a danger. Even if in this iteration this is not the case it is highly likely
+     * that business requirements will introduce multiple skills that impact the defence value of a character.
+     * Depending on the introduced skills the way skills are applied might require changes as it might be possible
+     * that skills are applied in a certain order, i.e. each skill will have a priority.
+     * Example:
+     * A. If a there is a new candidate skill, Elvish Gauntlet, that adds 10 to defence, this will not have a negative
+     * impact in the current implementation.
+     * B. If there is a new candidate skill, Fairy Necklace, that adds 10% to defence, it is imperative that is
+     * applied before the Magic Shield seeing that if the Magic Shield is applied first, due to the nature of the
+     * Fairy Necklace skill, this will result in an unwanted increase in defence.
+     */
     public function addDefenceValue(float $defenceValue, float $opponentAttackValue): float
     {
         return $defenceValue + $opponentAttackValue / 2;
--- src/Domain/Skill/RapidStrike.php
+++ src/Domain/Skill/RapidStrike.php
@@ -7,11 +7,18 @@ class RapidStrike extends AbstractSkill
 {
     public const IDENTIFIER = 'rapid_strike';
 
+    /*
+     * The introduction of new skills might ask for a change in the way skills are applied.
+     * Any other skill that increases attack value must be applied before the rapid strike skill.
+     */
     function addAttackValue(float $attackValue): float
     {
         return $attackValue * 2;
     }
 
+    /**
+     *
+     */
     function addDefenceValue(float $defenceValue, float $opponentAttackValue): float
     {
         return $defenceValue;
